d1e3f6f9021fd66fd279d7ff69db0b56e8db4d16
TingLiu <sixting19@outlook.com>
2024-02-24 00:41:54 -0800

    new changes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
-from .agent import Agent
-
+from flask import Flask, jsonify, request, render_template
+from agent import Agent
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/": {"origins": "*", "methods": ["GET", "POST"]}})
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,7 +13,12 @@ def index():
         # Your Python script's logic here, using user_input and making API calls
         agent = Agent()
         response = agent.execute(user_input)
-        return render_template('response.html', response=response['report'])
+        # return render_template('response.html', response=response['report'])
+        return jsonify({
+            'report': response['report'],
+            'stock_data': response['stock data'],
+            'all_news': response['all news']
+        })
     return render_template('index.html')  # A form for user input
 
 
